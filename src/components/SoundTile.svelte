80c92ddffcf6af47d358d37394997c205ae13b1d
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-01-15 07:00:12 +0100

    Add SoundTile-parent component

---
+++ src/components/SoundTile.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	let soundName: any
+	let soundFile: any
+	let isPlaying = false
+
+	function togglePlay(event: any) {
+		const audio = event.target.nextElementSibling as HTMLAudioElement
+		if (audio.paused) {
+			audio.play()
+			isPlaying = true
+		} else {
+			audio.pause()
+			isPlaying = false
+		}
+	}
+</script>
+
+<div>
+	<h3>{soundName}</h3>
+	<button on:click={togglePlay}>
+		{#if isPlaying}
+			Pause
+		{:else}
+			Play
+		{/if}
+	</button>
+	<audio controls autoplay>
+		<source src={soundFile} type="audio/mpeg" />
+	</audio>
+	<slot />
+</div>
