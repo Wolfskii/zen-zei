8e998dd891375f9e1b7a3857f731e8fe58240fdb
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-01-15 06:27:42 +0100

    Start on application main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,46 @@
 	import Header from '../components/Header.svelte'
 	import Footer from '../components/Footer.svelte'
 	import './styles.scss'
+	import RainVideo from '../lib/videos/rain.mp4'
 </script>
 
-<Header />
+<div id="app">
+	<Header />
+	<main>
+		<slot />
+	</main>
+	<Footer />
+</div>
 
-<main>
-	<slot />
-</main>
+<div id="overlay" />
 
-<Footer />
+<video autoplay muted loop id="bg-video">
+	<source src={RainVideo} type="video/mp4" />
+</video>
 
 <style>
+	#app {
+		position: relative;
+		overflow: hidden;
+		z-index: 2;
+	}
+
+	#overlay {
+		position: fixed;
+		right: 0;
+		bottom: 0;
+		min-width: 100%;
+		min-height: 100vh;
+		background-color: rgba(0, 0, 0, 0.2);
+		z-index: 1;
+	}
+
+	#bg-video {
+		position: fixed;
+		right: 0;
+		bottom: 0;
+		min-width: 100%;
+		min-height: 100vh;
+		z-index: 0;
+	}
 </style>
