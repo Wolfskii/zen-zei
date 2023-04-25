d8b5ecae5c87ba04e92c3d5598b510be184d9f2c
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-04-25 02:21:14 +0200

    Add thunder video

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 </div>
 
 <div class="sound-tiles">
-	<SoundTile soundName="Thunder" youTubeUrl="https://www.youtube.com/watch?v=HuT_5ckiq_s" />
+	<SoundTile soundName="Thunder" youTubeUrl="https://www.youtube.com/watch?v=NI0M03vCoXg" />
 	<SoundTile soundName="Rain" soundFile="src/lib/sounds/rain.wav" />
 </div>
 
