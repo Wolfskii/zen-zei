66c1a552d9214dce763fd47e1475df7f5728bdd7
Dawid Kaleta <dawid.kaleta@outlook.com>
2023-04-25 14:16:58 +0200

    Fix base in vite.config for repo path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
+	base: '/zen-zei/', // Change this to match your repo name
 	plugins: [sveltekit()]
 }
 
