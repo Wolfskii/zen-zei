140a912d6cfb7104579cb3bf05d658fa1151ea40
Wolfskii <dawid.kaleta@outlook.com>
2023-05-30 22:17:50 +0000

    Deploying to gh-pages from @ Wolfskii/zen-zei@9200b9aa79010fb0273a8b4ff6acb0ff66597fa7 🚀

--- output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.c37ed491.js","_app/immutable/chunks/index.b1f1455e.js","_app/immutable/chunks/paths.817cd703.js"];
-export const stylesheets = ["_app/immutable/assets/0.bb1794dc.css"];
-export const fonts = [];
--- output/server/nodes/2.js
+++
@@ -1,9 +0,0 @@
-import * as universal from '../entries/pages/_page.ts.js';
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export { universal };
-export const universal_id = "src/routes/+page.ts";
-export const imports = ["_app/immutable/nodes/2.10d72aca.js","_app/immutable/chunks/index.b1f1455e.js"];
-export const stylesheets = ["_app/immutable/assets/2.d7927e1c.css"];
-export const fonts = [];
